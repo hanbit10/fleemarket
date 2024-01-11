945e5a91343019a8342dcdff82b759e418f8d1a1
hanbit10 <chanbit10@gmail.com>
2024-01-11 21:33:54 +0100

    Build to publish on website

--- backend/server/server.mjs
+++ backend/server/server.mjs
@@ -3,15 +3,12 @@ dotenv.config();
 const { PORT, MONGODB_URI } = process.env;
 import app from "./app.mjs";
 import mongoose from "mongoose";
+import express from "express";
 
 const port = PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from Server!" });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
 
 mongoose.set("strictQuery", false);
@@ -22,3 +19,12 @@ mongoose
     console.log("Cannot connect to the database!", err);
     process.exit;
   });
+
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "../public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
