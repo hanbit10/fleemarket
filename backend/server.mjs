f690290521aef5f646d72df0b3b23fc0ee2cda2e
1bitcode <38600431+hanbit10@users.noreply.github.com>
2026-03-30 11:38:20 +0000

    update api

--- backend/server.mjs
+++ backend/server.mjs
@@ -17,7 +17,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB!"))
   .catch((err) => {
     console.log("Cannot connect to the database!", err);
-    process.exit;
+    process.exit(1);
   });
 
 app.listen(port, () => {
