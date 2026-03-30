55bac01b90813fcf615f21f06141d11b4663146f
1bitcode <38600431+hanbit10@users.noreply.github.com>
2026-03-30 11:20:01 +0000

    update for render

--- front-end/src/app/pages/login-page/login-page.component.ts
+++ front-end/src/app/pages/login-page/login-page.component.ts
@@ -26,22 +26,52 @@ export class LoginPageComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  loginUser() {
-    this._auth.loginUser(this.loginUserData.email, this.loginUserData.password).subscribe(data => {
-      console.log(data);
-      this.loggedIn = data.loggedIn;
+  // loginUser() {
+  //   this._auth.loginUser(this.loginUserData.email, this.loginUserData.password).subscribe(data => {
+  //     console.log(data);
+  //     this.loggedIn = data.loggedIn;
 
-      this.loggedIn ?
-      (
-        this.router.navigateByUrl('/welcome'), 
-        this._auth.loginUpdate(data.loggedIn, data.user), 
-        this.isDataInvalid = false
-      ) : (
-        alert("Please enter a correct ID and Password."),
-        this.router.navigateByUrl('/login'),
-        this.isDataInvalid = true
-      );
+  //     this.loggedIn ?
+  //     (
+  //       this.router.navigateByUrl('/welcome'), 
+  //       this._auth.loginUpdate(data.loggedIn, data.user), 
+  //       this.isDataInvalid = false
+  //     ) : (
+  //       alert("Please enter a correct ID and Password."),
+  //       this.router.navigateByUrl('/login'),
+  //       this.isDataInvalid = true
+  //     );
+  //   });
+  // }
+
+  loginUser() {
+    this._auth.loginUser(this.loginUserData.email, this.loginUserData.password).subscribe(n => {
+        console.log("Server Antwort:", n);
+
+        // Prüfung: Existiert 'n' und hat es die Eigenschaft 'loggedIn'?
+        if (n && typeof n.loggedIn !== 'undefined') {
+            this.loggedIn = n.loggedIn;
+
+            if (this.loggedIn) {
+                this.router.navigateByUrl("/welcome");
+                this._auth.loginUpdate(n.loggedIn, n.user);
+                this.isDataInvalid = false;
+            } else {
+                alert("Please enter a correct ID and Password.");
+                this.router.navigateByUrl("/login");
+                this.isDataInvalid = true;
+            }
+        } else {
+            // Falls der Server null oder ein leeres Objekt schickt
+            console.error("Ungültige Antwort vom Server");
+            this.isDataInvalid = true;
+            alert("Login fehlgeschlagen: Server antwortet nicht korrekt.");
+        }
+    }, error => {
+        // Falls die API einen 401, 404 oder 500 Fehler wirft
+        console.error("HTTP Fehler:", error);
+        alert("Verbindung zum Server fehlgeschlagen.");
     });
-  }
+}
 
 }
