54dddf1cc0faf68298ec8a9c42bc25eb16a01e50
1bitcode <38600431+hanbit10@users.noreply.github.com>
2026-04-01 07:08:11 +0000

    update mongodb

--- backend/server/api/middleware/upload.js
+++ backend/server/api/middleware/upload.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const diskStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "src/assets/images/productcardImages");
+    cb(null, "./dist/assets/images/productcardImages");
   },
   filename: (req, file, cb) => {
     const fileName = file.originalname;
