56f055b0d8b08fe3625ae15860ca05a9b013edc5
Hanbit Chang <chanbit10@gmail.com>
2026-08-17 09:34:18 +0200

    refactor: change the path to service to src

--- backend/src/api/routes/user.mjs
+++ backend/src/api/routes/user.mjs
@@ -1,43 +1,40 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import bcrypt from 'bcryptjs';
-import User from '../../../server/api/models/user.model.mjs';
+import bcrypt from "bcryptjs";
+import User from "../../../src/api/models/user.model.mjs";
 
 // Register new user
 // Password is hashed through bcrypto.
-router.post('/register', (req, res) => {
-  let { email, username, password }  = req.body;
+router.post("/register", (req, res) => {
+  let { email, username, password } = req.body;
 
-  User.findOne( { email: email }, async (err, user) => {
+  User.findOne({ email: email }, async (err, user) => {
     try {
       if (user) {
         return res.status(409).send("This email has already registered!");
-      } 
-      else {
-        const user = new User ({
+      } else {
+        const user = new User({
           email: email,
           username: username,
           password: password,
-          createdAt: Date.now()
-        })
+          createdAt: Date.now(),
+        });
 
         // Hash the password
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(user.password, salt, async (err, hash) => {
             if (err) throw err;
             user.password = hash;
-            await user.save()
+            await user.save();
             return res.json({ success: true });
-          })
-        })
+          });
+        });
       }
     } catch (err) {
       console.log(err);
       return res.json({ success: false });
     }
-  })
-})
+  });
+});
 
 export default router;
-
-
