087820d48c17990bcc2ef13de07a1b85adede95f
Hanbit Chang <chanbit10@gmail.com>
2026-08-17 09:54:05 +0200

    refactor: change path for productCard

--- backend/src/api/routes/productCard.mjs
+++ backend/src/api/routes/productCard.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
-import * as productCardRepository from "../../../src/api/models/productCard.model.mjs";
-import storage from "../../../src/api/middleware/upload.js";
+import * as productCardRepository from "../models/productCard.model.mjs";
+import storage from "../middleware/upload.js";
 
 // Get all cards
 router.get("/cards", async (req, res) => {
