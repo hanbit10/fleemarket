1b00b30906570960a07236c1800c134f52c1994b
Hanbit Chang <chanbit10@gmail.com>
2026-08-17 10:53:17 +0200

    refactor: change the path for urls

--- front-end/src/app/constants/urls.ts
+++ front-end/src/app/constants/urls.ts
@@ -1,4 +1,3 @@
-import { environment } from 'src/environments/environment';
-export const BASE_URL = environment.production 
-  ? 'https://everycent-backend.onrender.com' 
-  : 'http://localhost:3000';
+import { environment } from '../../environments/environment';
+
+export const BASE_URL = environment.apiUrl;
